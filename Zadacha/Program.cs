c1c025b49015fcb0770f7fbb5e9b77de94ffec27
Oiga Serik <Kharitonova.87@mail.ru>
2022-11-27 13:13:39 +0300

    txt

---
+++ Zadacha/Program.cs
@@ -0,0 +1,30 @@
+﻿/*Написать программу, которая из имеющегося массива строк формирует массив из строк,
+ длина которых меньше либо равна 3 символа.
+ */
+
+string[] array1 = new string[5] {"123", "45", "hello", "world", "6"};
+string[] array2 = new string[array1.Length];
+int count = 0;
+void ResultArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if(array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+ResultArray(array1, array2);
+PrintArray(array2);
+
